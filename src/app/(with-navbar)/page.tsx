2b55a291b997bfe6bc66fa015d66f081b0e52118
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-04-29 16:01:15 +0600

    project initialize

---
+++ src/app/(with-navbar)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-primary shadow-cardLightShadow">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
