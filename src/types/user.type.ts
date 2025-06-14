514a8c170860a9990d348d712e22569dd75d0cb0
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-06-14 14:09:23 +0600

    user type

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,5 +1,21 @@
 export type TUser = {
+  _id: string;
   name: string;
+  email: string;
+  role: "user" | "admin";
+  details: {
+    name: string;
+    education: string;
+    photoURL: string;
+    mobileNumber: string;
+    whatsAppNumber: string;
+    profession: string;
+    gender: string;
+    address: string;
+    dateOfBirth: string;
+  };
+  isDeleted: boolean;
+  __v: number;
 };
 
 export type TResponseUser = {
